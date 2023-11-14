329b3a161708f3761ec84b65c835d3c42eb6ebd2
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2023-11-14 11:41:28 +0300

    feat: complete task4 module4 v.0

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        first = first > second ? first : second;
+        first = first > third ? first : third;
+        System.out.println(first);
     }
 }
